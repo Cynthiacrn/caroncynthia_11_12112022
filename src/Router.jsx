97ad4b52129384c8d858b50dbc9df1278fc5fe93
Cynthiacrn <caron.cynthia@outlook.fr>
2023-01-08 18:14:18 +0100

    add router file

---
+++ src/Router.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+import Accommodation from "./pages/Accommodation";
+import Error from "./pages/Error";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/accomodation/:id" element={<Accommodation />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
+}
+
+export default Router;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./styles/style.css";
-import Home from "./pages/Home";
-import Accommodation from "./pages/Accommodation";
-import Error from "./pages/Error";
-import About from "./pages/About";
+import Router from "./Router";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -14,12 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/accomodation/:id" element={<Accommodation />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
+    <Router />
     <Footer />
   </BrowserRouter>
 );
